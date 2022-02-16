1e6e976686ef515b15329692d23d0c749122b57b
Intervention <riccardoapfelthalere1994@gmail.com>
2022-02-16 09:38:34 +0100

    Initial commit

---
+++ src/main/java/at/hakwt/swp4/springboothello/SpringNootHelloApplication.java
@@ -0,0 +1,13 @@
+package at.hakwt.swp4.springboothello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNootHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNootHelloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/hakwt/swp4/springboothello/SpringNootHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package at.hakwt.swp4.springboothello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNootHelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
